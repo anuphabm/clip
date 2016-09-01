b374cde7b0bf8ac3157c4da251bbbe18899c3a95
anuphab meekusol <anuphabm@gmail.com>
2016-09-01 16:11:29 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-var connectionString = process.env.DATABASE_URL || 'postgres://getcool:gdH98^]db@27.254.82.84:5432/getcool';
+var connectionString = process.env.DATABASE_URL || 'postgres://username:password@loclahost:5432/dbname';
 module.exports = connectionString;
